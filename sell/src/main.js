35acc1e12c0add5faa544c656613625aeb9e9d53
suzhihui <wisdom.su@163.com>
2016-11-21 18:40:42 +0800

    add files

--- sell/src/main.js
+++ sell/src/main.js
@@ -5,6 +5,8 @@ import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
 
+import 'common/stylus/index.scss';
+
 Vue.use(VueRouter);
 
 let app = Vue.extend(App);
