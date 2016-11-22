c571d4ba2c143f2f5e083a1d94c561f8f9e28c3f
suzhihui <wisdom.su@163.com>
2016-11-22 18:40:55 +0800

    add files

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,14 +17,20 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+
     </div>
   </div>
 </template>
@@ -34,6 +40,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
       }
     },
     created() {
@@ -46,8 +62,10 @@
   @import "../../common/stylus/mixin.scss";
 
   .header{
+    position: relative;
+    overflow: hidden;
     color: #fff;
-    background: #999;
+    background: rgba(7, 17, 27, 0.5);
     .content-wrapper{
       position: relative;
       padding: 24px 12px 18px 24px;
@@ -153,6 +171,25 @@
         right: 12px;
         top: 8px;
       }
+    }
+    .background{
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      filter:blur(10px);
+    }
+    .detail{
+      position: fixed;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background: rgba(7, 17, 27, 0.8);
+      top: 0;
+      left: 0;
     }
 
   }
