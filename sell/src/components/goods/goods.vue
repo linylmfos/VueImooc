90e5dcc86c313f7bb4c7be49fd2c568c60dbf13d
suzhihui <wisdom.su@163.com>
2016-11-23 22:53:43 +0800

    betterScroll

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -9,25 +9,24 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:food-wrapper>
       <ul>
-          <li v-for="item in goods" class="food-list">
+          <li v-for="item in goods" class="food-list food-list-hook">
             <h1 class="title">{{item.name}}</h1>
             <ul>
-              <li class="food-item" v-for="food in item.foods">
+              <li class="food-item border-1px" v-for="food in item.foods">
                 <div class="icon">
-                  <img alt="" :src="food.icon">
+                  <img width="57" height="57" :src="food.icon">
                 </div>
                 <div class="content">
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
                   <div class="extra">
-                    <span>月售{{food.sellCount}}份</span>
-                    <span>好评率{{food.rating}}%</span>
+                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                   </div>
                   <div class="price">
-                    <span>￥{{food.price}}</span>
-                    <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                    <span class="now">￥{{food.price}}</span>
+                    <spa class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                   </div>
                 </div>
               </li>
@@ -39,6 +38,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
 
   export default {
@@ -49,7 +50,8 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: []
       };
     },
     created() {
@@ -59,9 +61,30 @@
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
           // console.log(this.goods);
         }
       });
+    },
+    methods: {
+      _initScroll() {
+        this.meunScroll = new BScroll(this.$els.menuWrapper, {});
+
+        this.foodsScroll = new BScroll(this.$els.foodWrapper, {});
+      },
+      _calculateHeight() {
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      }
     }
   };
 </script>
@@ -127,6 +150,55 @@
         font-size: 12px;
         color: rgb(147, 153, 159);
         background: #f3f5f7;
+      }
+      .food-item{
+        display: flex;
+        padding: 18px;
+        @include border-1px(rgba(7,17,27,0.1));
+        &:last-child{
+          @include border-none();
+          margin-bottom: 0;
+        }
+        .icon{
+          flex: 0 0 57px;
+          margin-right: 10px;
+        }
+        .content{
+          float: 1;
+          .name{
+            margin: 2px 0 8px;
+            height: 14px;
+            line-height: 14px;
+            font-size: 14px;
+            color: rgb(7,17,27);
+          }
+          .desc, .extra{
+            line-height: 10px;
+            font-size: 10px;
+            color: rgb(147,153,159);
+          }
+          .desc{
+            margin-bottom: 8px;
+            line-height: 12px;
+          }
+          .extra{
+            .count{ margin-right: 12px;}
+          }
+          .price{
+            font-weight:700;
+            line-height: 24px;
+            .now{
+              margin-right: 8px;
+              font-size: 14px;
+              color: rgb(240,20,20);
+            }
+            .old{
+              text-decoration: line-through;
+              font-size: 10px;
+              color: rgb(147,153,159);
+            }
+          }
+        }
       }
     }
 
